5b5240c91ca29dadcb77ed298b7d335446118d4d
Kim Ji Hoon <ejvhfptmxm@nsu.ac.kr>
2023-04-05 00:44:47 +0900

    level 1

---
+++ 지훈/level 1/B10869.java
@@ -0,0 +1,16 @@
+package Backjoon;
+
+import java.util.Scanner;
+
+public class B10869 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt(), b = sc.nextInt();
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+
+    }
+}
