17ad1ae996fe599d59c129c998a8275387a908b3
komzihoon <128365461+komzihoon@users.noreply.github.com>
2023-04-05 00:52:42 +0900

    Add files via upload

---
+++ 지훈/B2557.java
@@ -0,0 +1,22 @@
+package Backjoon;
+
+import java.util.Scanner;
+
+public class B2557 {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
