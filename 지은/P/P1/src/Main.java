bf5ff2b823efac20dfbf291a0a0ea33f66464a3d
RRGJDS <qptqpt1432@naver.com>
2023-04-09 23:47:45 +0900

    P1

--- 지은/P/P1/src/Main.java
+++ 지은/P/P1/src/Main.java
@@ -1,5 +1,52 @@
+class D{
+    int num; //번호
+    static int hu=0; //사람수
+    int M=0; //돈
+}
+
+class Bus extends D{
+    int M=1500; //왜 빨간줄이야 상속했다메 했다메!!!!!!!!!!!!!!!
+    Bus(){}
+    Bus(int num){
+        this.num = num;
+    }
+    void sI(int num, int hu, int M){
+        String num = this.num; //학생은 되는데 왜 너는
+        int hu = this.hu;
+        int M = this.M;
+        System.out.printf("%s번의 승객은 %d명이고, 수입은 %d원 입니다.", num, hu, M);
+    }
+}
+class St{
+    static String name;
+    static int m;
+    static int num;
+
+    void takeBus (int num){
+        this.m -= 1500;
+        this.num=num;
+    }
+
+    void sI(){
+        String name = this.name;
+        int m = this.m;
+        System.out.printf("%s의 남은 돈은 %d원 입니다." , name, m);
+    }
+    St(){}
+    St(String name, int m){
+        this.name = name;
+        this.m=m;
+    }
+}
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        St SK = new St("Kim",10000);
+        St SL = new St("Lee",45000);
+        St SP = new St("Park",5000); //이거 근데 this로 하면 마지막만 저장되는데 어카지 : 일단 한 명만 살린다.....
+
+        Bus bus771 = new Bus(771);
+        SK.takeBus(bus771.num); //num안붙이고 가능한가
+        SK.sI();
+        bus771.sI();
     }
 }
