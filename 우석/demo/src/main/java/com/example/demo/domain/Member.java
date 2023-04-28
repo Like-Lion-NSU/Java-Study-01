5038f1e7ea88ccfe784093413d3e53e15ca9c65a
kws9208 <kws9208@gmail.com>
2023-04-28 23:40:16 +0900

    회원 도메인과 리포지토리 만들기

---
+++ 우석/demo/src/main/java/com/example/demo/domain/Member.java
@@ -0,0 +1,24 @@
+package com.example.demo.domain;
+
+public class Member {
+    // 데이터를 구분하기 위해 시스템이 저장하는 임의의 값
+    private Long id;
+    // 이름
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
