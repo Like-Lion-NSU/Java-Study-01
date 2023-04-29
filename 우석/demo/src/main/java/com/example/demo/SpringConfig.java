ef84c0e4fb9aeff98eb6ccb6a1548ca564734985
kws9208 <kws9208@gmail.com>
2023-04-30 00:25:55 +0900

    스프링 데이터 JPA

--- 우석/demo/src/main/java/com/example/demo/SpringConfig.java
+++ 우석/demo/src/main/java/com/example/demo/SpringConfig.java
@@ -3,37 +3,24 @@ package com.example.demo;
 import com.example.demo.repository.JpaMemberRepository;
 import com.example.demo.repository.MemberRepository;
 import com.example.demo.service.MemberService;
-import jakarta.persistence.EntityManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SpringConfig {
-    // 스프링 빈이 엔티티 매니저를 만들어 줌
-    private EntityManager em;
+    private final MemberRepository memberRepository;
 
-    // EntityManager 주입
     @Autowired
-    public SpringConfig(EntityManager em) {
-        this.em = em;
+    // 스프링 데이터 JPA가 만든 구현체 주입
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     // @Bean: 메서드를 실행하여 반환되는 객체를 스프링 빈에 등록
     @Bean
     public MemberService memberService(){
         // 스프링 빈에 등록된 memberRepository를 넣어줌
-        return new MemberService(memberRepository());
-    }
-
-    @Bean
-    public MemberRepository memberRepository(){
-//        return new MemoryMemberRepository();
-        // MemberRepository 구현체만 JDBC 리포지토리로 변경
-//        return new JdbcMemberRepository(dataSource);
-        // MemberRepository 구현체만 JDBC Template 리포지토리로 변경
-//        return new JdbcTemplateMemberRepository(dataSource);
-        // MemberRepository 구현체만 JPA 리포지토리로 변경
-        return new JpaMemberRepository(em);
+        return new MemberService(memberRepository);
     }
 }
