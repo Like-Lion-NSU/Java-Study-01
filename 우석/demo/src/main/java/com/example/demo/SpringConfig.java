19b174e7aef86d81ff468f41c7d5b05912f4149d
kws9208 <kws9208@gmail.com>
2023-04-29 22:30:41 +0900

    순수 JDBC

--- 우석/demo/src/main/java/com/example/demo/SpringConfig.java
+++ 우석/demo/src/main/java/com/example/demo/SpringConfig.java
@@ -1,13 +1,25 @@
 package com.example.demo;
 
+import com.example.demo.repository.JdbcMemberRepository;
 import com.example.demo.repository.MemberRepository;
-import com.example.demo.repository.MemoryMemberRepository;
 import com.example.demo.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+    // 스프링 빈이 데이터 소스를 만들어 줌
+    private DataSource dataSource;
+
+    // DataSource 주입
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     // @Bean: 메서드를 실행하여 반환되는 객체를 스프링 빈에 등록
     @Bean
     public MemberService memberService(){
@@ -17,6 +29,8 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        // MemberRepository 구현체만 JDBC 리포지토리로 변경
+        return new JdbcMemberRepository(dataSource);
     }
 }
