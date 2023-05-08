2047435ef3f1fbc712d9dc80e1695334ae93a1df
kws9208 <kws9208@gmail.com>
2023-05-08 15:03:45 +0900

    상품 엔티티 개발(비즈니스 로직 추가)

--- 우석/jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
+++ 우석/jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -2,6 +2,7 @@ package jpabook.jpashop.domain.item;
 
 import jakarta.persistence.*;
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -29,4 +30,23 @@ public abstract class Item {
     // 다대다 연관관계
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    // 비즈니스 로직
+    // 도메인 주도 설계, 엔티티 자체가 해결할 수 있는 문제는 엔티티 안에 비즈니스 로직 추가, 응집도 ↑
+
+    // 재고 증가
+    public void addStock(int quantity){
+        this.stockQuantity += quantity;
+    }
+
+    // 재고 감소
+    public void removeStock(int quantity){
+        int restStock = this.stockQuantity - quantity;
+        // 재고 체크
+        if(restStock < 0){
+            // 사용자 예외 NotEnoughStockException 발생
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity -= quantity;
+    }
 }
