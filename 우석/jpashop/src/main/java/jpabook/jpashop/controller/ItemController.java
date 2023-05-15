4426d252bdaa5655dbcc7ceb575e7402545ccdd9
kws9208 <kws9208@gmail.com>
2023-05-15 14:41:45 +0900

    상품 수정

--- 우석/jpashop/src/main/java/jpabook/jpashop/controller/ItemController.java
+++ 우석/jpashop/src/main/java/jpabook/jpashop/controller/ItemController.java
@@ -7,6 +7,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
@@ -41,5 +43,40 @@ public class ItemController {
         List<Item> items = itemService.findItems();
         model.addAttribute("items", items);
         return "items/itemList";
+    }
+
+    @GetMapping("/items/{itemId}/edit")
+    public String updateItemForm(@PathVariable("itemId") Long itemId, Model model){
+        // 예제를 단순화 하기위해 다운캐스팅
+        Book item = (Book) itemService.findItem(itemId);
+
+        BookForm form = new BookForm();
+        form.setId(item.getId());
+        form.setName(item.getName());
+        form.setPrice(item.getPrice());
+        form.setStockQuantity(item.getStockQuantity());
+        form.setAuthor(item.getAuthor());
+        form.setIsbn(item.getIsbn());
+
+        model.addAttribute("form", form);
+
+        return "items/updateItemForm";
+    }
+
+    @PostMapping("/items/{itemId}/edit")
+    // @ModelAttribute: 요청 파라미터를 받아서 객체 안에 넣어줌, 생략가능
+    public String updateItem(@PathVariable("itemId") String itemId, @ModelAttribute("form") BookForm form){
+        // 실무에서는 유저가 아이템에 대해 수정할 권한을 체크해주는 로직이 있어야 한다.
+        Book book = new Book();
+        book.setId(form.getId());
+        book.setName(form.getName());
+        book.setPrice(form.getPrice());
+        book.setStockQuantity(form.getStockQuantity());
+        book.setAuthor(book.getAuthor());
+        book.setIsbn(book.getIsbn());
+
+        itemService.saveItem(book);
+
+        return "redirect:/items";
     }
 }
