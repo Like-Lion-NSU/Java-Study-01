aa51d8ee805aa9454251eb2be4b86b9162e5fc9c
kws9208 <kws9208@gmail.com>
2023-06-20 15:07:22 +0900

    주문 조회 V6: JPA에서 DTO로 직접 조회, 플랫 데이터 최적화

--- 우석/jpashop/src/main/java/jpabook/jpashop/repository/order/query/OrderQueryDto.java
+++ 우석/jpashop/src/main/java/jpabook/jpashop/repository/order/query/OrderQueryDto.java
@@ -3,11 +3,14 @@ package jpabook.jpashop.repository.order.query;
 import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.OrderStatus;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Data
+// groupingBy 메서드를 호출할 때 orderId를 기준으로 하나로 묶어줌
+@EqualsAndHashCode(of = "orderId")
 public class OrderQueryDto {
     private Long orderId;
     private String name;
@@ -25,5 +28,14 @@ public class OrderQueryDto {
         this.orderStatus = orderStatus;
         this.address = address;
 //        this.orderItems = orderItems;
+    }
+
+    public OrderQueryDto(Long orderId, String name, LocalDateTime orderDate, OrderStatus orderStatus, Address address, List<OrderItemQueryDto> orderItems) {
+        this.orderId = orderId;
+        this.name = name;
+        this.orderDate = orderDate;
+        this.orderStatus = orderStatus;
+        this.address = address;
+        this.orderItems = orderItems;
     }
 }
